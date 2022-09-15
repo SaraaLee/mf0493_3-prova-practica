255537649f08b2e088b162868e2b16bb65a266d5
Sara Rodriguez <s.rodriguez91s@hotmail.com>
2022-09-15 16:43:56 +0200

    test solved

--- unit-testing/isogram.test.js
+++ unit-testing/isogram.test.js
@@ -1,3 +1,16 @@
 import {it, expect, describe} from 'vitest';
 import { isIsogram } from './isogram';
 
+it ("should return true for 'Dermatoglyphics' ", ()=>{
+    const result = isIsogram("Dermatoglyphics");
+    expect(result).toBe(true)
+})
+
+it ("should return false for 'aba' ", ()=>{
+    const result = isIsogram("aba");
+    expect(result).toBe(false)
+})
+it ("should return false for 'moOse' ", ()=>{
+    const result = isIsogram("moOse");
+    expect(result).toBe(false)
+})
